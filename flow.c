160d91af5b37db0a15fc9009f5c67e06e5f1f8f1
Anish Gupta <anish-gupta@outlook.com>
2020-06-29 22:54:59 +0100

    why doesnt this work

--- flow.c
+++ flow.c
@@ -1,17 +1,30 @@
 #include "navier-stokes/centered.h"
+#include "two-phase.h"
 #include "view.h"
 
-#define MAX_LEVEL 5
+#define MAX_LEVEL 7
 
 int main () {
   init_grid(1 << MAX_LEVEL);
   origin(-0.5, -0.5);
+
+  // set up fluids, doing this in init doesn't work
+
+  // Fluid 1 is water
+  rho1 = 1.;
+  mu1 = 1.;
+
+  // Fluid 2 is oil
+  rho2 = 0.9;
+  mu2 = 56.2;
+
   run();
 }
 
-event init(t = 0) {
 
-  mu=fm;
+
+event init(t = 0) {
+  fraction (f, 0.3-y);
 
   // flow out of the right -> into the left
   periodic(right);
@@ -27,16 +40,16 @@ event init(t = 0) {
   u.t[bottom] = dirichlet(0.);
 }
 
-event animationU (t += 0.02; t <= 1) {
+event animationU (t += 0.00002; t <= 0.002) {
   view ();
   clear();
   squares ("u.x", spread = -1, linear = true, map = cool_warm );
-  //draw_vof ("f", lc = {0.0,0.0,0.0}, lw=1);
+  draw_vof ("f", lc = {0.0,0.0,0.0}, lw=1);
   // box();
   save ("HorizontalVelocity.mp4");
 }
 
-event logfile (t+= 0.02; t <= 1) {
+event logfile (t+= 0.002; t <= 0.2) {
   // compute the speed
   scalar speed[];
   foreach ()
