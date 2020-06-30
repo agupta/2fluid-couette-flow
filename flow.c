b97ec8609bdaf123a8118e5ec492273f71d11204
Anish Gupta <anish-gupta@outlook.com>
2020-06-30 03:33:13 +0100

    data for varying rho2 and mu2

--- flow.c
+++ flow.c
@@ -1,10 +1,9 @@
 #include "navier-stokes/centered.h"
 #include "two-phase.h"
-#include "view.h"
 
-#define MAX_LEVEL 6
+#define MAX_LEVEL 5
 
-int main () {
+int main (int argc, char *argv[]) {
   init_grid(1 << MAX_LEVEL);
   origin(-0.5, -0.5);
 
@@ -14,9 +13,8 @@ int main () {
   rho1 = 1.;
   mu1 = 1.;
 
-  // Fluid 2 is oil
-  rho2 = 0.9;
-  mu2 = 56.2;
+  rho2 = atof(argv[1]);
+  mu2 = atof(argv[2]);
 
   run();
 }
@@ -39,27 +37,33 @@ event init(t = 0) {
   u.t[bottom] = dirichlet(0.);
 }
 
-event animationU (t += 0.00001; t <= 0.005) {
-  view ();
-  clear();
-  squares ("u.x", spread = -1, linear = true, map = cool_warm );
-  draw_vof ("f", lc = {0.0,0.0,0.0}, lw=1);
-  // box();
-  save ("HorizontalVelocity.mp4");
-}
+// event animationU (t += 0.00001; t <= 0.005) {
+//   view ();
+//   clear();
+//   squares ("u.x", spread = -1, linear = true, map = cool_warm );
+//   draw_vof ("f", lc = {0.0,0.0,0.0}, lw=1);
+//   // box();
+//   save ("HorizontalVelocity.mp4");
+// }
 
-event logfile (t += 0.00001; t <= 0.005) {
-  // compute the speed
-  scalar speed[]; 
-  foreach ()
-    // modulo floating point errors speed = u.x, but its cheating to assume that
-    speed[] = norm(u); // l2
-  // generate statistics
-  stats s = statsf(speed);
+// event logfile (t += 0.00001; t <= 0.005) {
+//   // compute the speed
+//   scalar speed[]; 
+//   foreach ()
+//     // modulo floating point errors speed = u.x, but its cheating to assume that
+//     speed[] = norm(u); // l2
+//   // generate statistics
+//   stats s = statsf(speed);
 
-  // log to file
+//   // log to file
+//   FILE * fp = fopen("flow.log", "a");
+//   fprintf (fp, "%g %d %g %g %g %g %g\n", t, i, dt, s.sum, s.max, s.min, interpolate(speed, 0., 0.3));
+//   // last one is speed at interface
+//   fclose(fp);
+// }
+
+event logfile (t = 1e+8) {
   FILE * fp = fopen("flow.log", "a");
-  fprintf (fp, "%g %d %g %g %g %g %g\n", t, i, dt, s.sum, s.max, s.min, interpolate(speed, 0., 0.3));
-  // last one is speed at interface
+  fprintf (fp, "%g %g %g\n", rho2, mu2, interpolate(u.x, 0., 0.3));
   fclose(fp);
 }
